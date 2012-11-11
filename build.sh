7b280882d7d0a0fed66603499b097a2a0a40e614
Pierre Gentile <pierre.gentile.perso@gmail.com>
2012-11-11 09:38:26 +0100

    Correction script de build

--- build.sh
+++ build.sh
@@ -7,26 +7,17 @@
 # Raise an error
 error()
 {
-	print "ERROR: $*" >&2
+	echo "ERROR: $*" >&2
 	exit 1
-}
-
-indent()
-{
-	typeset line
-	while read line
-	do
-		echo "   $line"
-	done
 }
 
 # Generate Makefiles
 echo "Generating Makefiles..."
-premake4 gmake | indent || error "Can't generate Makefiles"
+premake4 gmake || error "Can't generate Makefiles"
 
 # Build project
 echo "Building project..."
-make -j $CONCURRENT_BUILD_THREADS | indent || error "Can't build project"
+make -j $CONCURRENT_BUILD_THREADS || error "Can't build project"
 
 # End of build
 echo "Build done"
