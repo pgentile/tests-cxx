ec9cd194827ef1c6b79853f0970515bebcb96f9a
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-02-19 19:31:40 +0100

    Autodétection du nombre de cores sous MacOS

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # Build project from scratch
 
-# Concurrent build threads
-[[ -z "$CONCURRENT_BUILD_THREADS" ]] && CONCURRENT_BUILD_THREADS=2
 
 # Raise an error
 error()
@@ -11,13 +9,29 @@ error()
 	exit 1
 }
 
+
+# Number of parallel process
+
+if [[ -z "$PARALLEL" ]]
+then
+    # Nombre de cores sur MacOS
+    if [[ -x /usr/sbin/system_profiler ]]
+    then
+        PARALLEL=$(/usr/sbin/system_profiler -detailLevel full SPHardwareDataType | awk '/Total Number Of Cores/ { print $NF }')
+    fi
+fi
+
+[[ -z "$PARALLEL" ]] && PARALLEL=1
+
 # Generate Makefiles
+
 echo "Generating Makefiles..."
 premake4 gmake || error "Can't generate Makefiles"
 
 # Build project
-echo "Building project..."
-make -j $CONCURRENT_BUILD_THREADS $@ || error "Can't build project"
+
+echo "Building project ($PARALLEL process)..."
+make -j $PARALLEL $@ || error "Can't build project"
 
 # End of build
 echo "Build done"
